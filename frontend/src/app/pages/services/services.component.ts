ce796e900da913402aa6885302af0aaf56bda9ef
Nikolay Georgiev <nikolay.georgiev@nevexis.com>
2021-10-10 01:03:45 +0300

    FE create

--- frontend/src/app/pages/services/services.component.ts
+++ frontend/src/app/pages/services/services.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Service} from "./model/Service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'services-component',
@@ -6,9 +9,27 @@ import {Component} from '@angular/core';
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent {
+  services: Service[] = [];
   addingService: boolean = false;
+
+  serviceFormGroup: FormGroup = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.maxLength(30)]),
+    fromAmount: new FormControl("", [Validators.required, Validators.min(0)]),
+    toAmount: new FormControl(""),
+  });
+
+  constructor(private route: ActivatedRoute) {
+    console.log(this.route.snapshot);
+    console.log(this.route.snapshot.data);
+    this.services = this.route.snapshot.data.services;
+  }
 
   addNew() {
     this.addingService = true;
+    this.serviceFormGroup.patchValue({name: null, fromAmount: null, toAmount: null});
+  }
+
+  saveService() {
+    this.serviceFormGroup.patchValue({name: null, fromAmount: null, toAmount: null});
   }
 }
