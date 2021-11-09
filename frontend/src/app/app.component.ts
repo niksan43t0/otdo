ac2a606e837bc782cdda832538cfc59c851334b2
Nikolay Georgiev <nikolay.georgiev@nevexis.com>
2021-11-09 17:02:26 +0200

    paper moving

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, NgZone, OnDestroy, ViewChild} from '@angular/core';
+import {Subscription} from "rxjs";
+import {NgScrollbar} from "ngx-scrollbar";
+import {filter, map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'От&До';
+export class AppComponent implements AfterViewInit, OnDestroy {
+  private scrollSubscription = Subscription.EMPTY;
+  private mainMovingPaperElement: HTMLElement | null = null;
+  private vhPixels: number = 0;
+
+  // Get scrollbar component reference
+  @ViewChild(NgScrollbar) scrollbarRef!: NgScrollbar;
+
+  constructor(private zone: NgZone) {
+  }
+
+  ngAfterViewInit() {
+    this.updateVhPixels();
+    this.scrollSubscription = this.scrollbarRef.scrolled
+      .pipe(
+        filter(() => this.mainMovingPaperElement != null),
+        map((e: Event) => (e.target as HTMLElement).scrollTop),
+        filter((scrolled) => scrolled <= this.vhPixels),
+      ).subscribe((scrolled) => this.zone.run(() => {
+        const marginLeftInVw = scrolled / this.vhPixels * 100;
+        this.mainMovingPaperElement!!.style.marginLeft = `${marginLeftInVw}vw`;
+      }));
+  }
+
+  ngOnDestroy() {
+    this.scrollSubscription.unsubscribe();
+  }
+
+  onActivateRouter($event: any) {
+    this.mainMovingPaperElement = document.getElementById("main-page-moving-paper");
+  }
+
+  updateVhPixels() {
+    this.vhPixels = this.scrollbarRef.nativeElement.offsetHeight;
+  }
 }
