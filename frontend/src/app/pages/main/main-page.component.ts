2533c188ee7f5b0e4c289bdc821523bc3f63ce96
Nikolay Georgiev <nikolay.georgiev@nevexis.com>
2021-11-10 13:23:06 +0200

    viewchild

--- frontend/src/app/pages/main/main-page.component.ts
+++ frontend/src/app/pages/main/main-page.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -7,6 +7,9 @@ import {Router} from "@angular/router";
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent {
+  @ViewChild('mainPageMovingPaper') movingPaperRef!: ElementRef;
+  @ViewChild('mainPageMovingPaperInside') movingPaperInside!: ElementRef;
+
   constructor(private router: Router) {
   }
 
