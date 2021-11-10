62727fd9a372948696cb2851caa95c93e91e2d1e
Nikolay Georgiev <nikolay.georgiev@nevexis.com>
2021-11-10 13:55:30 +0200

    better animations

--- frontend/src/app/components/post/post.component.ts
+++ frontend/src/app/components/post/post.component.ts
@@ -1,52 +1,21 @@
 import {Component, ElementRef, Input, ViewChild} from '@angular/core';
-import {animate, keyframes, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
-  animations: [
-    trigger('fadeInLeft', [
-      transition('fadeStart => fadeEnd', animate('1000ms ease-in', keyframes([
-        style({
-          opacity: 0,
-          "-webkit-transform": 'translateX(-20px)',
-          transform: 'translateX(-20px)'
-        }),
-        style({
-          opacity: 1,
-          "-webkit-transform": 'translateX(0)',
-          transform: 'translateX(0)'
-        }),
-      ]))),
-    ]),
-    trigger('fadeInRight', [
-      transition('fadeStart => fadeEnd', animate('1000ms ease-in', keyframes([
-        style({
-          opacity: 0,
-          "-webkit-transform": 'translateX(20px)',
-          transform: 'translateX(20px)'
-        }),
-        style({
-          opacity: 1,
-          "-webkit-transform": 'translateX(0)',
-          transform: 'translateX(0)'
-        }),
-      ]))),
-    ])],
 })
 export class PostComponent {
   @Input() pictureOnRight: boolean = false;
+  @Input() triggerAnimation: boolean = false;
 
   @ViewChild('img') imgRef!: ElementRef;
   @ViewChild('text') textRef!: ElementRef;
-  fadeState: string = 'fadeStart';
 
   constructor() {
     setTimeout(() => {
-      this.fadeState = 'fadeEnd'; //detect on change ot @Input i also call dolniq red
-      this.imgRef.nativeElement.classList.remove("opacity-0");
-      this.textRef.nativeElement.classList.remove("opacity-0");
-    }, 2000)
+      this.imgRef.nativeElement.classList.add("fade-in");
+      this.textRef.nativeElement.classList.add("fade-in");
+    }, 1000)
   }
 }
