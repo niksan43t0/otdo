8bfc34c605e3031cd7810513371b775d03020178
Nikolay Georgiev <nikolay.georgiev@nevexis.com>
2021-11-11 12:48:24 +0200

    MainPageComponent

--- frontend/src/app/pages/main/main-page.component.ts
+++ frontend/src/app/pages/main/main-page.component.ts
@@ -1,4 +1,7 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
+import {ViewRangePixels} from "./model/ViewRangePixels";
+import {ActivatedRoute} from "@angular/router";
+import {Post} from "./model/Post";
 
 @Component({
   selector: 'main-page-component',
@@ -9,14 +12,10 @@ export class MainPageComponent {
   @ViewChild('mainPageMovingPaper') movingPaperRef!: ElementRef;
   @ViewChild('mainPageMovingPaperInside') movingPaperInside!: ElementRef;
   viewRangePixels: ViewRangePixels = new ViewRangePixels(0, 0);
-}
+  posts: Post[]
 
-export class ViewRangePixels {
-  from: number;
-  to: number;
-
-  constructor(from: number, to: number) {
-    this.from = from;
-    this.to = to;
+  constructor(private route: ActivatedRoute) {
+    console.log(this.route.snapshot.data.posts);
+    this.posts = this.route.snapshot.data.posts;
   }
 }
---
+++ frontend/src/app/pages/main/model/ViewRangePixels.ts
@@ -0,0 +1,9 @@
+export class ViewRangePixels {
+  from: number;
+  to: number;
+
+  constructor(from: number, to: number) {
+    this.from = from;
+    this.to = to;
+  }
+}
