dfcd9754b050f8a348aec88c10659dde34c66954
Nikolay Georgiev <nikolay.georgiev@nevexis.com>
2021-11-14 00:32:11 +0200

    currentPage underlined

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +7,28 @@ import {Component} from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  Page = Page;
+
+  constructor(private router: Router) {
+  }
+
+  get currentPage(): Page {
+    return this.router.url.startsWith("/services") ? Page.SERVICES :
+      this.router.url.startsWith("/machines") ? Page.MACHINES :
+        this.router.url.startsWith("/history") ? Page.HISTORY :
+          this.router.url.startsWith("/prices") ? Page.PRICES :
+            this.router.url.startsWith("/gallery") ? Page.GALLERY :
+              Page.NOT_FOUND;
+  }
 }
+
+export enum Page {
+  HOME = "HOME",
+  SERVICES = "SERVICES",
+  MACHINES = "MACHINES",
+  HISTORY = "HISTORY",
+  PRICES = "PRICES",
+  GALLERY = "GALLERY",
+  NOT_FOUND = "NOT FOUND",
+}
+
