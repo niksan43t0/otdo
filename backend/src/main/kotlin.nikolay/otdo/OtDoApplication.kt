047d5520fcf928d79bfe7fe9df3f9b4c3768f4a8
niksan43t0 <niksata__@abv.bg>
2023-02-18 12:45:12 +0200

    working now

--- backend/src/main/kotlin.nikolay/otdo/OtDoApplication.kt
+++ backend/src/main/kotlin.nikolay/otdo/OtDoApplication.kt
@@ -2,11 +2,9 @@ package otdo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
-@EnableScheduling
 @EnableTransactionManagement
 open class OtDoApplication
 
